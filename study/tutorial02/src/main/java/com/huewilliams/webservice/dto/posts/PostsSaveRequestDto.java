108b86a3e53a4f24703212635b4b89bb84aaddc5
huewilliams <4879sinwoo@naver.com>
2019-09-18 15:17:21 +0900

    ADD: post DTO

---
+++ study/tutorial02/src/main/java/com/huewilliams/webservice/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.huewilliams.webservice.dto.posts;
+
+import com.huewilliams.webservice.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
