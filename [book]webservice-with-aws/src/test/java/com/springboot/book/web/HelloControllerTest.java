9f2014c548cd54a45ab14047ed7d719445930c49
huewilliams <4879sinwoo@naver.com>
2020-03-17 16:50:26 +0900

    Add - GET/helloDto controller test code

--- [book]webservice-with-aws/src/test/java/com/springboot/book/web/HelloControllerTest.java
+++ [book]webservice-with-aws/src/test/java/com/springboot/book/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 // 테스트 실행시 SpringRunner 라는 스프링 실행자를 사용한다.(Spring과 JUnit 간의 연결)
@@ -29,5 +29,19 @@ public class HelloControllerTest {
                 // mvc.perform의 결과 검증(HTTP Header의 status가 200인지 검증)
                 .andExpect(content().string(hello));
                 // mvc.perform의 결과 검증(응답 본문의 내용이 "hello"인지 검증)
+    }
+
+    @Test
+    public void return_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name) // API 테스트 시 요청 파라미터 설정(단, String 값만 허용)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+                // jsonPath : JSON 응답값을 필드별로 검증하는 메소드($를 기준으로 필드명 명시)
     }
 }
