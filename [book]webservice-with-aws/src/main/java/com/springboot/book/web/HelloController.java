ad81c8a22ecf4102c21b0a670bc54675a6dc4b00
huewilliams <4879sinwoo@naver.com>
2020-03-17 16:49:31 +0900

    Add - GET/helloDto controller(HelloDtoResponse 객체 반환)

--- [book]webservice-with-aws/src/main/java/com/springboot/book/web/HelloController.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.springboot.book.web;
 
+import com.springboot.book.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // JSON을 반환하는 컨트롤러로 설정
@@ -9,5 +11,14 @@ public class HelloController {
     @GetMapping("/hello") // HTTP GET 요청을 받는 API로 설정
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        // @RequestParam : 외부에서 API로 넘긴 파라미터를 가져옴.
+        // 외부에서 "name" 으로 넘긴 파라미터를 String name에 저장함.
+        return new HelloResponseDto(name, amount);
+
     }
 }
