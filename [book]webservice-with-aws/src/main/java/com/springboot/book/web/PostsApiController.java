ef8c19564f7b32d8d75becaeef77dc960767af65
huewilliams <4879sinwoo@naver.com>
2020-03-18 18:55:32 +0900

    Add - POST/posts controller(게시물 저장 API)

---
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/PostsApiController.java
@@ -0,0 +1,22 @@
+package com.springboot.book.web;
+
+import com.springboot.book.service.posts.PostsService;
+import com.springboot.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+    }
+}
