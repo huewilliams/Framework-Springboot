a151ec9e5afbdf6f974ecf6c27b38ef4c0274bdd
huewilliams <4879sinwoo@naver.com>
2020-03-18 21:22:39 +0900

    Add - update method to Posts entity

--- [book]webservice-with-aws/src/main/java/com/springboot/book/domain/posts/Posts.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/domain/posts/Posts.java
@@ -35,4 +35,9 @@ public class Posts {
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
