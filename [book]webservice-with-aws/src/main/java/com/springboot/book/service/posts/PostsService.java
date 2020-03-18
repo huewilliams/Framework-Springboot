7c850a8c48c71e555e3dba6d2b3e4c1cbd60c545
huewilliams <4879sinwoo@naver.com>
2020-03-18 21:25:35 +0900

    Add - update and findById method to posts service class

--- [book]webservice-with-aws/src/main/java/com/springboot/book/service/posts/PostsService.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.springboot.book.service.posts;
 
+import com.springboot.book.domain.posts.Posts;
 import com.springboot.book.domain.posts.PostsRepository;
+import com.springboot.book.web.dto.PostsResponseDto;
 import com.springboot.book.web.dto.PostsSaveRequestDto;
+import com.springboot.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        // update 기능에서 쿼리를 날리는 부분이 없다. => JPA의 영속성 컨텍스트(엔티티를 영구 저장하는 환경)
+        // 더티 체킹 : JPA 엔티티 매니저가 Entity 값을 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경을 반영함.
+
+        return  id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
