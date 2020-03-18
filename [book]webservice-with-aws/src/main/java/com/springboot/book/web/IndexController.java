09a4b4728d973fdfa5522e795003abe538796f00
huewilliams <4879sinwoo@naver.com>
2020-03-18 22:10:52 +0900

    Add - GET/ controller(index page)

---
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.springboot.book.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
