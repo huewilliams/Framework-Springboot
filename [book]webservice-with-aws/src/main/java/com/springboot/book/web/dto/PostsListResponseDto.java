88235032d22110903290461378400de5dd31eea2
huewilliams <4879sinwoo@naver.com>
2020-03-18 22:53:48 +0900

    Add - posts find all dto class

---
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/dto/PostsListResponseDto.java
@@ -0,0 +1,19 @@
+package com.springboot.book.web.dto;
+
+import com.springboot.book.domain.posts.Posts;
+
+import java.time.LocalDateTime;
+
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getUpdatedDate();
+    }
+}
