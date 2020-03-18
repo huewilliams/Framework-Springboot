2b92c482f78bf5482e5774f4effcf9382a19ba9b
huewilliams <4879sinwoo@naver.com>
2020-03-18 22:56:24 +0900

    Fix - GET/ controller posts List 템플릿으로 전달

--- [book]webservice-with-aws/src/main/java/com/springboot/book/web/IndexController.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/IndexController.java
@@ -1,13 +1,26 @@
 package com.springboot.book.web;
 
+import com.springboot.book.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        // model : 서버 템플릿 엔진에서 사용하는 객체를 저장함.
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
