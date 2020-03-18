ae12ff9cdb0b6a341b43186a0dfe22bf9778e390
huewilliams <4879sinwoo@naver.com>
2020-03-18 22:54:20 +0900

    Add - findAllDesc method to posts repository

--- [book]webservice-with-aws/src/main/java/com/springboot/book/domain/posts/PostsRepository.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/domain/posts/PostsRepository.java
@@ -1,9 +1,14 @@
 package com.springboot.book.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // Repository :  DB Layer 접근자
 // JpaRepository<Entity 클래스, PK 타입>를 상속하면 자동으로 기본적인 CRUD 메소드가 생성됨.
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") // 스프링 부트가 제공하지 않는 메소드는 쿼리로 작성할 수 있다.
+    List<Posts> findAllDesc();
 }
