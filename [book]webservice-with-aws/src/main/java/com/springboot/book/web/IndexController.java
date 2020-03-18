48a82b4f25ecde1fe123c26af618b54f63e8dc31
huewilliams <4879sinwoo@naver.com>
2020-03-18 23:04:54 +0900

    Add - GET/posts/update controller(posts update page)

--- [book]webservice-with-aws/src/main/java/com/springboot/book/web/IndexController.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.springboot.book.web;
 
 import com.springboot.book.service.posts.PostsService;
+import com.springboot.book.web.dto.PostsListResponseDto;
+import com.springboot.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,4 +26,14 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    private String postsUpdate(@PathVariable Long id,
+                               Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
 }
