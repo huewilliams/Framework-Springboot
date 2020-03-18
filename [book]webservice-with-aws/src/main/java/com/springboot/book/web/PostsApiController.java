07915497c383ce78bac0a5536000fe2229e4252c
huewilliams <4879sinwoo@naver.com>
2020-03-18 21:26:10 +0900

    Add - PUT/posts and GET/posts controller

--- [book]webservice-with-aws/src/main/java/com/springboot/book/web/PostsApiController.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/PostsApiController.java
@@ -1,12 +1,11 @@
 package com.springboot.book.web;
 
 import com.springboot.book.service.posts.PostsService;
+import com.springboot.book.web.dto.PostsResponseDto;
 import com.springboot.book.web.dto.PostsSaveRequestDto;
+import com.springboot.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +17,18 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
 
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto) {
+
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+
+        return postsService.findById(id);
     }
 }
