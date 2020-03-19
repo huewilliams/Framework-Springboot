c07fd2f53552c8b5b11c47d1502474242bcb8d18
huewilliams <4879sinwoo@naver.com>
2020-03-19 19:02:16 +0900

    Add - user role enum class

---
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/domain/user/Role.java
@@ -0,0 +1,16 @@
+package com.springboot.book.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    // 스프링 시큐리티에서는 퀀한 코드에 항상 ROLE_이 앞에 와야한다.
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
