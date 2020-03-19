d56484bda25120c71cc23d91afa29f28ed7acfda
huewilliams <4879sinwoo@naver.com>
2020-03-19 19:02:31 +0900

    Add - user entity class

---
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/domain/user/User.java
@@ -0,0 +1,51 @@
+package com.springboot.book.domain.user;
+
+import com.springboot.book.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    // JPA로 DB에 저장시 Enum 값을 어떤 형태로 저장할지 결정함.
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
