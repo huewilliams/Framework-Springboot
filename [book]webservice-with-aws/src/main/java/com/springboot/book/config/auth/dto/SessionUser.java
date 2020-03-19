76e44eac2b90e7350bde1972651a696e09165150
huewilliams <4879sinwoo@naver.com>
2020-03-19 21:27:49 +0900

    Add - User session dto class

---
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.springboot.book.config.auth.dto;
+
+import com.springboot.book.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
