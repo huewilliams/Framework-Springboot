ac5f3bb29952c1a8e472b698ebfc40d470f6ccbc
huewilliams <4879sinwoo@naver.com>
2020-03-19 23:10:54 +0900

    Fix - JpaAuditing 활성화 옵션 별도 파일로 분리

--- [book]webservice-with-aws/src/main/java/com/springboot/book/Application.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/Application.java
@@ -2,9 +2,7 @@ package com.springboot.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // 스프링부트 자동 설정, Bean read/write
 public class Application {
     public static void main(String args[]) {
