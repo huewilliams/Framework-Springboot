23a44f0964ebdeb38efacdffd9a31cc0119fcdf3
huewilliams <4879sinwoo@naver.com>
2020-03-19 21:29:40 +0900

    Use - user session

--- [book]webservice-with-aws/src/main/java/com/springboot/book/web/IndexController.java
+++ [book]webservice-with-aws/src/main/java/com/springboot/book/web/IndexController.java
@@ -1,7 +1,7 @@
 package com.springboot.book.web;
 
+import com.springboot.book.config.auth.dto.SessionUser;
 import com.springboot.book.service.posts.PostsService;
-import com.springboot.book.web.dto.PostsListResponseDto;
 import com.springboot.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,16 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         // model : 서버 템플릿 엔진에서 사용하는 객체를 저장함.
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
