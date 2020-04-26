474d846bee38592cbab036eaa38fe72889f52c87
huewilliams <4879sinwoo@naver.com>
2020-04-26 19:04:52 +0900

    Add - Main module(fetch multiple Applications)

---
+++ [example]gradle-multi-module/main/src/main/java/com/example/multimodule/MainApplication.java
@@ -0,0 +1,12 @@
+package com.example.multimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example.multimodule.api.*", "com.example.multimodule.admin.*"})
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
