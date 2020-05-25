0de7c66b4a02487e594fb2e29ce0a2f5b65e38fa
huewilliams <4879sinwoo@naver.com>
2020-05-25 17:20:12 +0900

    EDIT : error typing fix

--- [book]webservice-with-aws/scripts/deploy.sh
+++ [book]webservice-with-aws/scripts/deploy.sh
@@ -12,11 +12,11 @@ CURRENT_PID=$(pgrep -fl webservice-with-aws | grep jar | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "kill -15 "CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
