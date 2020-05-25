1f8f967d23bd2636466b082dcaa08c683b1a5c3d
huewilliams <4879sinwoo@naver.com>
2020-05-25 18:37:33 +0900

    EDIT : jar running script

--- [book]webservice-with-aws/scripts/deploy.sh
+++ [book]webservice-with-aws/scripts/deploy.sh
@@ -33,4 +33,4 @@ echo "> $JAR_NAME 실행"
 
 cd $REPOSITORY
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-oauth.properties,classpath:/applicatin-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,$REPOSITORY/application-oauth.properties,$REPOSITORY/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
