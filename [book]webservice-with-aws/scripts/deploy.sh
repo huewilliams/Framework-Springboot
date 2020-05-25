8fbf28060ee84fcaa867fc13b378d54b7a051005
huewilliams <4879sinwoo@naver.com>
2020-05-25 18:33:45 +0900

    EDIT : deploy script

--- [book]webservice-with-aws/scripts/deploy.sh
+++ [book]webservice-with-aws/scripts/deploy.sh
@@ -31,4 +31,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
+cd $REPOSITORY
+
 nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-oauth.properties,classpath:/applicatin-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
