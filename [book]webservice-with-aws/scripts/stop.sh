9bece3b41d2defb5748744253167d2280b57929e
huewilliams <4879sinwoo@naver.com>
2020-05-27 16:46:09 +0900

    EDIT: script typing

--- [book]webservice-with-aws/scripts/stop.sh
+++ [book]webservice-with-aws/scripts/stop.sh
@@ -9,7 +9,7 @@ IDLE_PORT=$(find_idle_port)
 echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-id [ -z ${IDLE_PID} ]
+if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
