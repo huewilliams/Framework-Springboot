353f5e3e0d312d3a5922d1e0b65561ef51ea0709
huewilliams <4879sinwoo@naver.com>
2020-05-27 17:11:53 +0900

    EDIT : miss typing

--- [book]webservice-with-aws/scripts/profile.sh
+++ [book]webservice-with-aws/scripts/profile.sh
@@ -4,7 +4,7 @@
 function find_idle_profile() {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-    if [ ${RESPONSE_CODE} -ge 400] # 400 보다 크면(40x, 50x 에러)
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면(40x, 50x 에러)
     then
       CURRENT_PROFILE=real2
     else
