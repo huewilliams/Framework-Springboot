c87c6d881704fa17f54a293ceeea4f19ecebfd71
huewilliams <4879sinwoo@naver.com>
2020-07-05 22:06:11 +0900

    ADD: point entity & repository

---
+++ [example]embedded-redis/src/main/java/com/example/redis/domain/Point.java
@@ -0,0 +1,33 @@
+package com.example.redis.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@RedisHash("point")
+public class Point implements Serializable {
+
+    @Id
+    private String id;
+    private Long amount;
+    private LocalDateTime refreshTime;
+
+    @Builder
+    public Point(String id, Long amount, LocalDateTime refreshTime) {
+        this.id = id;
+        this.amount = amount;
+        this.refreshTime = refreshTime;
+    }
+
+    public void refresh(long amount, LocalDateTime refreshTime) {
+        if (refreshTime.isAfter(this.refreshTime)) {
+            this.amount = amount;
+            this.refreshTime = refreshTime;
+        }
+    }
+}
---
+++ [example]embedded-redis/src/main/java/com/example/redis/domain/PointRedisRepository.java
@@ -0,0 +1,6 @@
+package com.example.redis.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PointRedisRepository extends CrudRepository<Point, String> {
+}
