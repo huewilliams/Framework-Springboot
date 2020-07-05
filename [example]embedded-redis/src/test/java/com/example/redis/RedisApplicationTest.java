7770942ebf0444ce284c709119fefdfc67b47aec
huewilliams <4879sinwoo@naver.com>
2020-07-05 22:06:28 +0900

    ADD: redis entity save and find test

---
+++ [example]embedded-redis/src/test/java/com/example/redis/RedisApplicationTest.java
@@ -0,0 +1,49 @@
+package com.example.redis;
+
+import com.example.redis.domain.Point;
+import com.example.redis.domain.PointRedisRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisApplicationTest {
+
+    @Autowired
+    private PointRedisRepository pointRedisRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        pointRedisRepository.deleteAll();
+    }
+
+    @Test
+    public void save_and_find() {
+        //given
+        String id = "huewilliams";
+        LocalDateTime refreshTime = LocalDateTime.now();
+        Point point = Point.builder()
+                .id(id)
+                .amount(1000L)
+                .refreshTime(refreshTime)
+                .build();
+
+        //when
+        pointRedisRepository.save(point);
+
+        //then
+        Point savedPoint = pointRedisRepository.findById(id).get();
+        assertThat(savedPoint.getAmount()).isEqualTo(1000L);
+        assertThat(savedPoint.getRefreshTime()).isEqualTo(refreshTime);
+        assertThat(savedPoint.getId()).isEqualTo(id);
+    }
+
+}
