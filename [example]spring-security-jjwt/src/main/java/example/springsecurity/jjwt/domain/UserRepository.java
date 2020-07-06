5a7f32b633ef98e7ba40581fa6c776f22d24493f
huewilliams <4879sinwoo@naver.com>
2020-07-06 12:03:06 +0900

    ADD: user repository

---
+++ [example]spring-security-jjwt/src/main/java/example/springsecurity/jjwt/domain/UserRepository.java
@@ -0,0 +1,10 @@
+package example.springsecurity.jjwt.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
