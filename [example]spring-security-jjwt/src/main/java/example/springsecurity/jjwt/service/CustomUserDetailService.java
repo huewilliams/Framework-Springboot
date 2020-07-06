5424eb455647541291bc5620713be81cf4ade4c6
huewilliams <4879sinwoo@naver.com>
2020-07-06 12:05:52 +0900

    ADD: custom user details service

---
+++ [example]spring-security-jjwt/src/main/java/example/springsecurity/jjwt/service/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package example.springsecurity.jjwt.service;
+
+import example.springsecurity.jjwt.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("cannot find user."));
+    }
+}
